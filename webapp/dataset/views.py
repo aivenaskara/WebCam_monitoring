0e09c766351ca4aad48720fbc76755b1d787699f
Kirill Mentyukov <rogue5@mail.ru>
2021-11-18 16:28:26 +0300

    Fix bags in dataset/views

--- webapp/dataset/views.py
+++ webapp/dataset/views.py
@@ -1,8 +1,8 @@
 import base64
 import pickle
 
-from flask import Blueprint, render_template, request, url_for
-from PIL import Image
+from flask import Blueprint, redirect, render_template, request, url_for
+from flask_login import current_user
 from webapp.dataset.forms import DetectionON
 from webapp.dataset.models import Photo
 
@@ -11,13 +11,15 @@ blueprint = Blueprint('dataset', __name__, url_prefix='/dataset')
 
 @blueprint.route('/')
 def index():
+    if current_user.is_anonymous:
+        return redirect(url_for('index'))
     title = "Архив записей"
     page = request.args.get('page', 1, type=int)
     records_list = Photo.query.filter(Photo.created_on).order_by(Photo.created_on.desc()).paginate(page, 10, False)
     next_url = url_for('dataset.index', page=records_list.next_num) \
-        if records_list.has_next else None
+        if records_list.has_next else '?page='+str(page)
     prev_url = url_for('dataset.index', page=records_list.prev_num) \
-        if records_list.has_prev else None
+        if records_list.has_prev else '?page=1'
     """
     form = DetectionON()
     if form.detection_on:
@@ -37,6 +39,8 @@ def index():
 
 @blueprint.route('/dataset/<int:photo_id>')
 def show_photo(photo_id):
+    if current_user.is_anonymous:
+        return redirect(url_for('index'))
     cam_photo = Photo.query.filter(Photo.id == photo_id).first()
     load_photo = pickle.loads(cam_photo.photo)
     base64img = "data:image/png;base64,"+base64.b64encode(load_photo.getvalue()).decode('ascii')
